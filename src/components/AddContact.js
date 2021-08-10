5b95cbb8eebb528124945c96fbe1f3f459926d86
SM farhad Hossain <farhad1556@gmail.com>
2021-08-10 18:26:05 +0600

    Localstorage

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,20 +1,45 @@
 import React from 'react'
 
 class AddContact extends React.Component{
+    state={
+        name:"",
+        email:""
+    };
+    add = (e)=>{
+        e.preventDefault();
+        if(this.state.name ==="" || this.state.email ===""){
+            alert("All Fields are mandatory");
+            return;
+        }
+        this.props.addContactHandler(this.state);
+        this.setState({name:"", email:""});
+        //console.log(this.state);
+    }
+
     render(){
         return (
             <div className="ui main">
                 <h2> Add Contact</h2>
-                <form class="ui form">
-                    <div class="field">
+                <form className="ui form" onSubmit={this.add}>
+                    <div className="field">
                         <label>Name</label>
-                        <input type="text" name="name" placeholder="Name" />
+                        <input 
+                            type="text" 
+                            name="name" 
+                            placeholder="Name"
+                            value={this.state.name}
+                            onChange={(e)=>this.setState({name:e.target.value})}/>
                     </div>
-                    <div class="field">
+                    <div className="field">
                         <label>Email</label>
-                        <input type="text" name="Email" placeholder="Email" />
+                        <input 
+                            type="text" 
+                            name="Email" 
+                            placeholder="Email"
+                            value={this.state.email}
+                            onChange={(e)=>this.setState({email:e.target.value})}/>
                     </div>
-                    <button class="ui button" type="submit">Submit</button>
+                    <button className="ui button" type="submit">Submit</button>
                 </form>
             </div>
         );
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -4,15 +4,15 @@ const ContactCard = (props) => {
     //console.log(props);
     const {id, name, email} = props.contact; 
 return (
-    <div class="item">
-        <div class="ui grid">
-            <div class="four wide column">
-            <img className="ui avatar image" src={user} alt="user {id}"/>
+    <div className="item">
+        <div className="ui grid">
+            <div className="four wide column">
+            <img className="ui avatar image" src={user} alt={'user-'+id} />
             </div>
-            <div class="four wide column">{name}</div>
-            <div class="four wide column">{email}</div>
-            <div class="four wide column">
-                <i className="right floated trash alternate outline icon red"></i>
+            <div className="four wide column">{name}</div>
+            <div className="four wide column">{email}</div>
+            <div className="four wide column">
+                <i className="ui primary right floated trash alternate outline icon red"></i>
             </div>
         </div> 
     </div>
